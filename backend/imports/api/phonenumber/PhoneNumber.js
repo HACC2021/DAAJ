c04f956fca0a440c9456830a1f1a1479f8e1b341
alyssia-chen <84742988+alyssia-chen@users.noreply.github.com>
2021-11-02 01:09:03 -1000

    Put a deactivated schema for phone numbers for future ref

--- backend/imports/api/phonenumber/PhoneNumber.js
+++ backend/imports/api/phonenumber/PhoneNumber.js
@@ -1,17 +1,49 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
-
+import SimpleSchema from "simpl-schema";
+import { check } from "meteor/check";
+import { Tracker } from "meteor/tracker";
 
 export const PhoneNumbers = new Mongo.Collection( 'phoneNumbers' )
+
+// SimpleSchema.defineValidationErrorTransform((error) => {
+//   const ddpError = new Meteor.Error(error.message);
+//   ddpError.error = "validation-error";
+//   ddpError.details = error.details;
+//   return ddpError;
+// });
+
+// // Define a Schema for PhoneNumbers:
+// const PhoneNumbersSchema = new SimpleSchema({
+//    name: String,
+//    number: Number
+//   }, { check }, {
+//     clean: {
+//       autoConvert: true,
+//       extendAutoValueContext: {},
+//       filter: false,
+//       getAutoValues: true,
+//       removeEmptyStrings: false,
+//       removeNullsFromArrays: false,
+//       trimStrings: true,
+//     },
+//     humanizeAutoLabels: false,
+//     requiredByDefault: true, //false
+//   })
+
+// const PhoneNumbersArgSchema = PhoneNumbersSchema;
 
 // Meteor methods:
 Meteor.methods({
   addPhoneNumber( data ){
+    console.log("In Meteor method addPhoneNumber");
+    // PhoneNumbersArgSchema.validate(PhoneNumbersSchema.clean(data)); // Example of how to validate
     PhoneNumbers.insert({
       name: data.name,
       number: data.number
     }, err => {
       if (err){
+        console.log("an error with validation");
         return err
       } else {
         return null
