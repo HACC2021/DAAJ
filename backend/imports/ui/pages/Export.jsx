2e971a7fb3c6219a0b921ae5263cee6f13313271
alyssia-chen <84742988+alyssia-chen@users.noreply.github.com>
2021-11-07 11:22:16 -1000

    Export all seals [WIP]

--- backend/imports/ui/pages/Export.jsx
+++ backend/imports/ui/pages/Export.jsx
@@ -7,6 +7,12 @@ import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
 import { Stuffs } from '../../api/stuff/Stuff';
 
+import { Turtles } from '../../api/turtle/Turtle';
+import { Birds } from '../../api/bird/Bird';
+import { Seals } from '../../api/seal/Seal';
+import { Others } from '../../api/other/Other';
+
+
 // Create a schema to specify the structure of the data to appear in the form.
 const formSchema = new SimpleSchema({
 });
@@ -38,10 +44,10 @@ class Export extends React.Component {
       <Grid container centered>
         <Grid.Column>
           <Header as="h2" textAlign="center">Export</Header>
-          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.exportData(data, fRef)} >
+          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.exportSealData(data, fRef)} >
             <Segment>
-              <SubmitField value='Submit'/>
-              <ErrorsField/>
+              <SubmitField value='Submit' />
+              <ErrorsField />
             </Segment>
           </AutoForm>
         </Grid.Column>
@@ -49,15 +55,40 @@ class Export extends React.Component {
     );
   }
 
-  exportData(data, ref) {
-    const rows = [
-      ["name1", "city1", "some other info"],
-      ["name2", "city2", "more info"]
-    ];
+  exportSealData(data, ref) {
+    // Problems with: "ID Temp (Bleach #)" , "# of Volunteers Engaged"
+    // Headers copied from Sightings Data Template
+    let theHeaders = ["Date", "Time", "Ticket Number", "Hotline Operator Initials", "Ticket Type", "Observer", "Observer Contact Number", "Observer Initials", "Observer Type", "Sector", "Location", "Location Notes", "Seal Present?", "Size", "Sex", "Beach Position", "How Identified?", "ID Temp", "Tag Number", "Tag Side", "Tag Color", "ID Perm", "Molt", "Additional Notes on ID", "ID Verified By", "Seal Logging", "Mom & Pup Pair", "SRA Set Up", "SRA Set By", "Num of Volunteers Engaged", "Seal Depart Info Avail?", "Seal Departed Date", "Seal Departed Time", "Number of Calls Received", "Other Notes"]
+
+    // Get everything in the Seals collection (can do some filtering too)
+    let sealsToExport = Seals.find({}).fetch(); // an array of seal objects
+
+    // For each seal, put its fields into an array 
+    let rows = []
+    // Push the headers on 
+    rows.push(theHeaders);
+    for (let index = 0; index < sealsToExport.length; index++) {
+      // Initialize an empty array for this seal
+      rows.push([]);
+      // Grab the fields from the collection to put into the array
+      rows[index + 1].push(sealsToExport[index].ObserverName);
+      rows[index + 1].push(sealsToExport[index].ObserverPhone);
+    }
+
+    console.log("exporting result: " + JSON.stringify(rows));
+    // const rows = [
+    //   theHeaders,
+    //   ["name1", "city1", "some other info"],
+    //   ["name2", "city2", "more info"]
+    // ];
 
     let csvContent = "data:text/csv;charset=utf-8,"
       + rows.map(e => e.join(",")).join("\n");
+      
+    console.log("csvContent:\n" + csvContent);
 
+    // encodeURI doesn't encode: , / ? : @ & = + $ # ()
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURI <- figure out how to encode it by replacing it or using encodeURIComponent?
     var encodedUri = encodeURI(csvContent);
     window.open(encodedUri);
   }
