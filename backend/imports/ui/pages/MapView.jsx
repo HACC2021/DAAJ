dff70356660d14fdbe17386f116dbf085064f252
Jolie <joliec@hawaii.edu>
2021-11-06 18:10:46 -1000

    Map View Base

--- backend/imports/ui/pages/MapView.jsx
+++ backend/imports/ui/pages/MapView.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import { Grid, Segment, Header } from 'semantic-ui-react';
-import { AutoForm, ErrorsField, NumField, SelectField, SubmitField, TextField } from 'uniforms-semantic';
 import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
+import { GoogleMap, InfoWindow, Marker, LoadScript } from '@react-google-maps/api';
+import ReportItem from '../components/ReportItem';
+import { Container, Grid, Table, Header, Loader } from 'semantic-ui-react';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
 import { Stuffs } from '../../api/stuff/Stuff';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
-
-// Create a schema to specify the structure of the data to appear in the form.
-const formSchema = new SimpleSchema({
-  name: String,
-  quantity: Number,
-  condition: {
-    type: String,
-    allowedValues: ['excellent', 'good', 'fair', 'poor'],
-    defaultValue: 'good',
-  },
-});
+import { Turtles } from '../../api/turtle/Turtle';
+import { Birds } from '../../api/bird/Bird';
+import { Seals } from '../../api/seal/Seal';
+import { Others } from '../../api/other/Other';
 
 const mapStyles = {
   marginTop: "-10px",
@@ -27,43 +22,129 @@ const mapStyles = {
 const defaultCenter = {
   lat: 20.3069, lng: -157.5583
 }
-
-
-const bridge = new SimpleSchema2Bridge(formSchema);
-
-/** Renders the Page for adding a document. */
 class MapView extends React.Component {
-
-  
-  // On submit, insert the data.
-  submit(data, formRef) {
-    const { name, quantity, condition } = data;
-    const owner = Meteor.user().username;
-    Stuffs.collection.insert({ name, quantity, condition, owner },
-      (error) => {
-        if (error) {
-          swal('Error', error.message, 'error');
-        } else {
-          swal('Success', 'Item added successfully', 'success');
-          formRef.reset();
-        }
-      });
+  constructor(props) {
+    super(props);
+    this.state = {
+      pin: {},
+      pinPressed: false,
+      isOpen: false
+    };
   }
-
-  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
   render() {
-    let fRef = null;
-    return (
-      <LoadScript
-      googleMapsApiKey='AIzaSyDy4lATc_hd8VHpkRBfDYUgfD3pGNQtdXA'>
-       <GoogleMap
-         mapContainerStyle={mapStyles}
-         zoom={7}
-         center={defaultCenter}
-       />
-    </LoadScript>
-    );
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
   }
-}
 
-export default MapView;
+  getReports() {
+    /*
+        console.log("turtles");
+        console.log(this.props.turtles);
+        console.log("birds");
+        console.log(this.props.birds);
+        console.log("seals");
+        console.log(this.props.seals);
+        console.log("others");
+        console.log(this.props.others);
+        */
+
+        // adding fields to each array to indicate the animal of the report
+        const turtles = this.props.turtles.map(report => ({...report, type: "Turtle"}));
+        const birds = this.props.birds.map(report => ({...report, type: "Bird"}));
+        const seals = this.props.seals.map(report => ({...report, type: "Seal"}));
+        const others = this.props.others.map(report => ({...report, type: "Other"}));
+        // stitching arrays of objects of reports for each animal type together, to map it to ReportItem
+        return [...turtles, ...birds, ...seals, ...others];
+      }
+
+      pinPressed(report) {
+        this.setState({pin: report, pinPressed: true});
+      }
+
+      handleToggleOpen = () => {
+        this.setState({
+          isOpen: true
+        });
+      }
+      
+      handleToggleClose = () => {
+        this.setState({
+          isOpen: false
+        });
+      }
+  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
+  renderPage() {
+    let fRef = null;
+
+    return (
+      <Grid>
+        {this.state.pinPressed ? 
+        <Grid.Column width={4}>
+          <Grid.Row> <Header as='h1'>{this.state.pin.Animal}</Header> </Grid.Row>
+          <Grid.Row> <Header as='h3'>Observed: {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'long', day: 'numeric' }).format(this.state.pin.DateObjectObserved)} 
+          {" "} {new Intl.DateTimeFormat('en-US', { hour: 'numeric', minute: 'numeric', timeZone: 'HST' }).format(this.state.pin.DateObjectObserved)} HST </Header> </Grid.Row>
+          <Grid.Row> <Header as='h3'>Location: {this.state.pin.LocationName}</Header> </Grid.Row>
+          <Grid.Row> <Header as='h3'>Status: {this.state.pin.status}</Header> </Grid.Row>
+        </Grid.Column> :
+         <Grid.Column width={4}>
+         <Grid.Row> <Header as='h1'>Click on pin to get started! </Header> </Grid.Row>
+        </Grid.Column>
+         }
+        <Grid.Column width={12}>
+          <LoadScript
+            googleMapsApiKey='AIzaSyDy4lATc_hd8VHpkRBfDYUgfD3pGNQtdXA'>
+           <GoogleMap
+              mapContainerStyle={mapStyles}
+              zoom={7}
+              center={defaultCenter}
+            >
+            {this.getReports().map(report => {
+              return <Marker onClick={() => {this.pinPressed(report); this.handleToggleOpen()}} position={{lat: report.xLatitude, lng:report.xLongitude}} key={report._id}>
+     </Marker>
+           })}
+            </GoogleMap>
+          </LoadScript>
+        </Grid.Column>
+      </Grid>
+    );
+  }}
+
+// Require an array of Stuff documents in the props.
+MapView.propTypes = {
+  stuffs: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+// withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe(Stuffs.userPublicationName);
+  // Determine if the subscription is ready
+  const ready = subscription.ready();
+  // Get the Stuff documents
+  const stuffs = Stuffs.collection.find({}).fetch();
+  const turtleSubscription = Meteor.subscribe('TurtlesCollection');
+  const turtleReady = turtleSubscription.ready();
+  const turtles = Turtles.find({}).fetch();
+  const birdSubscription = Meteor.subscribe('BirdsCollection');
+  const birdReady = birdSubscription.ready();
+  const birds = Birds.find({}).fetch();
+  const sealSubscription = Meteor.subscribe('SealsCollection');
+  const sealReady = sealSubscription.ready();
+  const seals = Seals.find({}).fetch();
+  const otherSubscription = Meteor.subscribe('OthersCollection');
+  const otherReady = otherSubscription.ready();
+  const others = Others.find({}).fetch();
+
+  return {
+    stuffs,
+    ready,
+    turtleReady,
+    turtles,
+    birdReady,
+    birds,
+    sealReady,
+    seals,
+    otherReady,
+    others
+  };
+})(MapView);
